507a9a11aac56f8dd916c396d42ac8108dd53af8
Piyush Saini <sainipiyush8300@gmail.com>
2026-07-12 23:49:06 +0530

    chore: look for .env in parent folder if not present in Cwd

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env", env_file_encoding="utf-8", case_sensitive=True, extra="ignore"
+        env_file=(".env", "../.env"), env_file_encoding="utf-8", case_sensitive=True, extra="ignore"
     )
 
     PROJECT_NAME: str = "AI Skin Intelligence & Personalized Skincare Planner"
